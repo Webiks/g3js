45ae3eec160f38f7ac96e1afae91f7738266ac65
yuvalbl <yuval.bar.levi@gmail.com>
2017-05-15 11:20:59 +0300

    Add graph config and data options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  catindex = 1;
-  updateFlag = false;
+  updateFlag = [];  // set a new reference to force graph component to update changes
+  displayDisabledColumns = true;
   // value - set category value (column height, default 0)
   // id - bar rect element id. setDisplay function based on id
   // text - set label
   // css - set bar class
   myData = [
-    {id: '1', text: 'cat1', value: 13},
+    {id: '1', text: 'cat1', value: 13, color: ''},
     {text: 'cat2', value: 10},
     {text: 'cat3', value: 12},
     {text: 'cat4', value: 3},
@@ -22,25 +22,55 @@ export class AppComponent {
   ];
   // id - graph id function based on id
   // css - set bar class
-  // responsivefy - override responsivefy function
   chartConfig = {
     id: '',
     css: '',
-    responsivefy: () => {},
-    setY: () => {}
+    transitionTime: '',
+    filterDataFunction: (d) => this.displayDisabledColumns || d.addToDOM !== false,
+    skipTransitionOnce: false,
+    margin: {top: 30, right: 20, bottom: 60, left: 40},
+    colorFunction: (d, i) => (d.value > 10) ? 'green' : 'red',
+    axis: {
+      x: {
+        display: true,
+        diagonal: true
+      },
+      y: {
+        display: true,
+      }
+    },
+  };
+  // graph click
+  graphClick(event) {
+    if (event.target.tagName === 'rect') {
+      this.barClick(event);
+    } else {
+      console.log('some click event', event);
+    }
   }
-  hide() {
-    this.myData.splice(this.catindex, 1);
-    this.updateFlag = true;
-
+  // graph - bar click
+  barClick(event) {
+    console.log(event.target);
+    let newClass;
+    if (!event.data.css) {
+      event.data.css = '';
+    }
+    if (event.data.css.indexOf('disabled') === -1) {
+      newClass = event.data.css + ' disabled';
+      event.data['addToDOM'] = false;
+    } else {
+      newClass = event.data.css.replace(' disabled', '');
+      event.data['addToDOM'] = undefined;
+    }
+    event.data.css = newClass;
+    if (this.displayDisabledColumns) {
+      this.chartConfig.skipTransitionOnce = true;
+    }
+    this.updateFlag = [];
   }
-  changeCss() {
-    this.myData[this.catindex]['css'] = 'myCss';
-    this.updateFlag = true;
 
-  }
-
-  updateFlagReset() {
-    this.updateFlag = false;
+  toggleDisabledDisplay() {
+    this.displayDisabledColumns = !this.displayDisabledColumns ;
+    this.updateFlag = [];
   }
 }
--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,20 +1,24 @@
 import {
   Component, ElementRef, Input, Output, OnChanges, SimpleChanges, ViewChild,
-  ViewEncapsulation, EventEmitter
+  ViewEncapsulation, EventEmitter, DoCheck
 } from '@angular/core';
 import {D3, D3Service,} from 'd3-ng2-service';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class BarChartComponent implements OnChanges {
+  private static DEFAULT = {
+    transitionTime: 700,
+    margin: {top: 30, right: 0, bottom: 60, left: 40}
+  };
   private static d3: D3;
   @Input() data: any[];
-  @Input() update: boolean;
-  @Output() onUpdateReset = new EventEmitter<boolean>();
+  @Input() config: any;
+  @Input() update: any;
+  @Output() onClick = new EventEmitter<any>();
 
   @ViewChild('graphContainer') parentElement: ElementRef;
   // This function is based on code published by brendan sudol
@@ -44,13 +48,20 @@ export class BarChartComponent implements OnChanges {
       svg.attr('height', Math.round(targetWidth / aspect));
     }
   }
+
+
+  static addOnClick(element, context) {
+    element.on('click', (data, index, elements) => {
+      context.onClick.emit({data, index, elements, 'target': elements[index]});
+    });
+  }
   constructor(d3Service: D3Service) {
     BarChartComponent.d3 = d3Service.getD3();
     // console.log(this.parentNativeElement);
   }
-
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges() {
     if (this.data) {
+      let transitionDuration = this.getTransitionDuration();
       const d3 = BarChartComponent.d3;
       const d3ParnetElement = d3.select(this.parentElement.nativeElement);
       const svg = d3ParnetElement.html('')
@@ -58,44 +69,48 @@ export class BarChartComponent implements OnChanges {
         .attr('class', 'graph graph--bar-chart')
         .call(BarChartComponent.responsivefy);
       let parentMatric: ClientRect = (<HTMLElement>svg.node()).getBoundingClientRect();
-      let margin = {top: 30, right: 0, bottom: 60, left: 40};
+      let margin = this.config.margin || BarChartComponent.DEFAULT.margin;
       let width = parentMatric.width - margin.left - margin.right;
       let height = parentMatric.height - margin.top - margin.bottom;
 
       let mainG = svg.append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`)
 
-      BarChartComponent.responsivefy(svg)
-      // svg.attr('viewbox', null)
-      // todo: add override support
+
+      let data = this.data;
+      if (this.config.filterDataFunction && typeof this.config.filterDataFunction === 'function') {
+        data = data.filter((d) => this.config.filterDataFunction(d));
+      }
       // y scale
       let maxYValue = d3.max(this.data, d => d.value);
       let yScale = d3.scaleLinear()
         .domain([0, maxYValue])
         .range([height, 0]);
       let yAxis = d3.axisLeft(yScale)
+      // todo: extern tick?
         .tickPadding(20);
       mainG.call(yAxis);
       // todo: add override support
       // x scale
       let xScale = d3.scaleBand()
         .padding(0.2)
-        .domain(this.data.map(d => d.text))
+        .domain(data.map(d => d.text))
         .range([0, width]);
       let xAxis = d3.axisBottom(xScale);
-
-      // todo: add override support
+      // add axis click event
       // todo: add directions support?
       mainG.append('g')
         .attr('transform', `translate(0, ${height})`)
         .call(xAxis)
         .selectAll('text')
-        .style('text-anchor', 'middle');
 
-      //add bars
-      // todo: add override support
+        .style('text-anchor', 'middle')
+      // add labels click event
+      BarChartComponent.addOnClick(mainG.selectAll('text'), this);
+
+
       let bar = mainG.selectAll('rect')
-        .data(this.data)
+        .data(data)
         .enter()
         .append('rect')
         .attr('class', (d, k) => {
@@ -103,20 +118,31 @@ export class BarChartComponent implements OnChanges {
           const indexClass = `graph__bar--index${k}`;
           const inputClass = (d.css) ? ` ${d.css}` : '';
           return `graph__bar ${indexClass}${inputClass}`;
-        });
+        })
+      bar.attr('fill', (d, k) => this.config.colorFunction(d, k))
+      // add bar click event
+      BarChartComponent.addOnClick(bar, this);
 
-      // todo: add override transition support
       bar.attr('x', d => xScale(d.text))
         .attr('width', d => xScale.bandwidth())
         .attr('y', height)
         .attr('height', 0)
         .transition()
-        .duration(700)
+        .duration(transitionDuration)
         .ease(d3.easeCircle)
         .attr('height', d => height - yScale(d.value))
         .attr('y', d => yScale(d.value))
-
-      this.onUpdateReset.emit();
     }
+  }
+
+  getTransitionDuration() {
+    let duration = BarChartComponent.DEFAULT.transitionTime;
+    if (this.config.skipTransitionOnce) {
+      duration = 0;
+      this.config.skipTransitionOnce = false;
+    } else if (this.config.transitionTime) {
+      duration = this.config.transitionTime;
+    }
+    return duration;
   }
 }
